f63390446054517862e4f1aea1e3a6972ab6a707
dduan97 <dduan97@gmail.com>
2016-12-30 22:46:11 -0500

    testing a message response

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,11 +1,22 @@
 // router for /message routes
 
+var twilio = include("twilio");
+
 var router = require("express").Router();
 
 // handles POST to /message/
 // Twilio will POST here for an incoming message
 router.post("/", function(req, res){
-    res.send("hello again!");
+
+    // we need to get the message text.
+
+    // try responding
+    var resp = new twilio.TwimlResponse();
+
+    resp.message("please work!");
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(resp.toString());
 })
 
 module.exports = router;
--- helpers/twilio.js
+++ helpers/twilio.js
@@ -1,15 +1,17 @@
 var client = require("twilio")(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-// callback should take in <error> and <responseData>
-// <responseData>: a JavaScript object containing data received from Twilio.
-// A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-// http://www.twilio.com/docs/api/rest/sending-sms#example-1
-module.exports.send_sms = function(number, message, callback){
-    var message_details = {
-        to: number,
-        from: "+18482063592",
-        body: message
-    };
+// // callback should take in <error> and <responseData>
+// // <responseData>: a JavaScript object containing data received from Twilio.
+// // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+// // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+// module.exports.send_sms = function(number, message, callback){
+//     var message_details = {
+//         to: number,
+//         from: "+18482063592",
+//         body: message
+//     };
 
-    client.sendMessage(message_details, callback);
-}
+//     client.sendMessage(message_details, callback);
+// }
+
+module.exports = client;
