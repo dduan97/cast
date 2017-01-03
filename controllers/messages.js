6531fea28dde1b9a2bfad87f68324522fb0beed5
dduan97 <dduan97@gmail.com>
2017-01-03 17:01:44 -0500

    error handling and wrapping twiml response into a helper

--- controllers/messages.js
+++ controllers/messages.js
@@ -4,6 +4,7 @@ var twilio = require("twilio");
 var router = require("express").Router();
 var geocoder = require("geocoder");
 
+var twilio_helpers = __include("helpers/twilio.js");
 var dark_sky = __include("helpers/dark-sky.js");
 var ds_client = new dark_sky.DarkSky(process.env.DARK_SKY_KEY);
 
@@ -24,10 +25,19 @@ router.post("/", function(req, res){
         if (error) {
             __logger.error("Error with geocoder api:");
             __logger.error(error);
+            twilio_helpers.send_twiml(res, ["Error with message body " + message_body + ". (geocoding error response)"]);
             return;
         }
 
         var results = data.results;
+
+        // if the results list is empty
+        if (results.length == 0) {
+            __logger.info("No geocoding results for " + message_body);
+            twilio_helpers.send_twiml(res, ["Location not found: " + message_body + ". Please try again"]);
+            return;
+        }
+
         var coords = results[0].geometry.location;
 
         // this gets the hourly blurb for a certain location
@@ -35,18 +45,12 @@ router.post("/", function(req, res){
 
             if (error) {
                 __logger.error("Error with DarkSky API: ${error}");
+                twilio_helpers.send_twiml(res, ["Error retrieving DarkSky forecast for " + message_body]);
                 return;
             }
 
-            // try responding
-            var resp = new twilio.TwimlResponse();
-
-            resp.message(message_body + ": " + body.hourly.summary);
-
-            __logger.info("responding with %s", resp.toString());
-
-            res.writeHead(200, {'Content-Type': 'text/xml'});
-            res.end(resp.toString());
+            var reply_string = message_body + ": " + body.hourly.summary;
+            twilio_helpers.send_twiml(res, [reply_string]);
 
         });
 
--- helpers/twilio.js
+++ helpers/twilio.js
@@ -1,4 +1,5 @@
 var client = require("twilio")(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+var twilio = require("twilio");
 
 // // callback should take in <error> and <responseData>
 // // <responseData>: a JavaScript object containing data received from Twilio.
@@ -14,4 +15,21 @@ var client = require("twilio")(process.env.TWILIO_SID, process.env.TWILIO_TOKEN)
 //     client.sendMessage(message_details, callback);
 // }
 
-module.exports = client;
+// function to send one TwiML responses
+// <res> should be the response object from the hook
+// <msgs> should be a list of strings corresponding to the list of message 
+// verbs wanted in the response
+module.exports.send_twiml = function(res, msgs){
+    var resp = new twilio.TwimlResponse();
+
+    msgs.forEach(function(element){
+        resp.message(element);
+    });
+
+    __logger.info("responding with %s", resp.toString());
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(resp.toString());
+}
+
+module.exports.twilio = client;
